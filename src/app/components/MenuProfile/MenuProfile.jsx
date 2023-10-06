c47b8dd82446427b8f29bf009087103558bdc5a9
Lalo Freak <lalofreak.dev@gmail.com>
2023-10-06 15:04:22 -0300

    auth flow rectified

--- src/app/components/MenuProfile/MenuProfile.jsx
+++ src/app/components/MenuProfile/MenuProfile.jsx
@@ -1,22 +1,28 @@
-import { useSelector } from 'react-redux';
 import s from './MenuProfile.module.css';
-import defaultUserIcon from '../../../assets/images/png/user-icon.png'
+import { useSelector } from 'react-redux';
 import { logout } from '../../../middlewares/redux/actions/auth';
 import { $d } from '../../../functions';
+import defaultUserIcon from '../../../assets/images/png/user-icon.png'
 
 export const MenuProfile = () => {
   const { profilePic } = useSelector(state => state.currentUser);
   
   function showMenu() {
-    $d('#menu-profile-container').classList.toggle(s.showMenu)
+    $d('#menu-profile-list-container').style.display='block'
   }
 
+  document.addEventListener('click', (e) => {
+    if(!$d('#menu-profile-container').contains(e.target)) {
+      $d('#menu-profile-list-container').style.display='none';
+    };
+  });
+
   return (
-    <div className={s.container}>
+    <div className={s.container} id='menu-profile-container'>
       <div className={s.profilePicContainer} onClick={showMenu}>
         <img src={profilePic ? profilePic : defaultUserIcon} alt="" width="100%" />
       </div>
-      <ul className={s.menuProfileContainer} id='menu-profile-container'>
+      <ul className={s.menuProfileContainer} id='menu-profile-list-container'>
         <li>Cuenta</li>
         <li>Perfil</li>
         <li className={s.separator} onClick={() => logout()}>Cerrar sesión</li>
