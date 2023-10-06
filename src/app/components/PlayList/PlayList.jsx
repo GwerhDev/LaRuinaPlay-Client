7a50b7b6d2180db1a54775c4ef8c5dbeddf59e4b
Lalo Freak <lalofreak.dev@gmail.com>
2023-10-06 20:42:07 -0300

    styling playlist

--- src/app/components/PlayList/PlayList.jsx
+++ src/app/components/PlayList/PlayList.jsx
@@ -19,13 +19,24 @@ export const PlayList = () => {
   return (
     <section className="section-container">
       <div className="primary-container">
-        <Navigator/>
-        <div className="header-playlist">
-          <img src={RenderDriveImage(details.imageVisor)} className={s.headerImage} alt="" width="100%"/>
+        <Navigator />
+        <div className="secundary-container">
+          <div className="header-playlist" style={{ backgroundImage: `url(${RenderDriveImage(details?.imageVisor)})`}}>
+            <div className={s.headerContainer}>
+              <div className={s.contImg}>
+                <img className={s.imgList} src={RenderDriveImage(details?.imageSlider)} alt="default" height='100%' />
+              </div>
+              <div className={s.titleList}>
+                <span className={s.contentType}>{details?.contentType}</span>
+                <span className={s.title}>{details?.title}</span>
+                <span className={s.tracks}>{details?.items?.length? details?.items?.length + " pistas" : null}</span>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="main-section">
 
-        </div> 
+        </div>
       </div>
     </section>
   )
