12c72b2cd01dc3393083063c8825ff920e8384d0
GwerhDev <gwerh.dev@gmail.com>
2024-04-18 11:46:33 -0400

    library: create playlist logic

--- src/middlewares/redux/actions/playlist.js
+++ src/middlewares/redux/actions/playlist.js
@@ -5,7 +5,7 @@ import { options } from "../../helpers";
 export function createPlaylist(formData) {
   return async function (dispatch) {
     try {
-      const response = await axios.post(`${URL_API}/laruinaplay/playlist/create`, formData, options());
+      const response = await axios.post(`${URL_API}/subscriptor/playlist/create`, formData, options());
       return response.data;
     } catch (error) {
       console.error(error);
